4b9c6cd054a148f0fda2d937ad67f1e614bee69d
Albert Barreto <albert.barreto@outlook.com>
2020-05-23 01:21:49 -0300

    search engines information

--- js/bora.js
+++ js/bora.js
@@ -1,6 +1,6 @@
 // Store percentage values when user makes changes
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     Object.keys(SEARCH_ENGINE_URLS).forEach((engine) => {
         let input = document.getElementById(engine);
 
@@ -78,5 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         $('.percentage').text(totalPercentage);
     }
+
+    $('[data-toggle="popover"]').popover();
 
 });
